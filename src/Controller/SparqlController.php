ef30878a1f2455ee5418b7ad4f41e494a866692e
Jason Carvalho <jase@jasoncarvalho.com>
2021-03-17 09:56:18 +0000

    initial dev

--- src/Controller/SparqlController.php
+++ src/Controller/SparqlController.php
@@ -31,10 +31,57 @@ class SparqlController extends AbstractActionController
     }
 
     public function queryAction() {
-
-        return new ViewModel([
-            'message'   =>  'hello from the SPARQL controller'
-        ]);
+        $user_id = $this->currentUser()->getId();
+        $id = (int) $this->params()->fromRoute('id', 0);
+        //FIXME - Also make sure this is a stream dataset that we are retrieving.
+        $dataset = $this->_dataset_repository->findDataset($id);
+        //$permissions = $this->_repository->findDatasetPermissions($id);
+        $message = "Dataset: " . $id;
+        $actions = [];
+        $can_view = $this->_permissionManager->canView($dataset,$user_id);
+        $can_read = $this->_permissionManager->canRead($dataset,$user_id);
+        $can_edit = $this->_permissionManager->canEdit($dataset,$user_id);
+        $streamExists = $this->_repository->getStreamExists($dataset->uuid);
+        $keys = [];
+        //$userHasKey = false; //Does the user have a key on this stream (ie can they make SPARQL read queries)?
+        $userHasKey = $this->_keys_repository->userHasDatasetKey($user_id,$dataset->id);
+        if ($can_view && $can_read && $userHasKey) {
+            $docCount = 0;
+            $keys = [];
+            if ($streamExists) {
+                $docCount = $this->_repository->getDocCount($dataset->uuid)['totalDocs'];
+            }
+            else {
+                $this->flashMessenger()->addErrorMessage('Data API not yet activated');
+                return $this->redirect()->toRoute('stream', ['action'=>'details']);
+            }
+            $actions = [
+                'label' => 'Actions',
+                'class' => '',
+                'buttons' => [
+                ]
+            ];
+            return new ViewModel([
+                'message' => $message,
+                'doc_count' => $docCount,
+                'keys' => $keys,
+                'dataset' => $dataset,
+                'stream_url' => $this->_repository->getApiReadHref($dataset->uuid),
+                'read_url' => $this->_repository->getApiReadHref($dataset->uuid),
+                'write_url' => $this->_repository->getApiWriteHref($dataset->uuid),
+                'browse_url' => $this->_repository->getApiBrowseHref($dataset->uuid),
+                'api_home' => $this->_repository->getApiHome(),
+                'features' => $this->datasetsFeatureManager()->getFeatures($id),
+                'actions' => $actions,
+                'can_edit' => $can_edit,
+                'can_read' => $can_read,
+                'user_has_key' => $userHasKey,
+            ]);
+        }
+        else{
+            $this->flashMessenger()->addErrorMessage('You do not have a read key on this dataset');
+            return $this->redirect()->toRoute('dataset', ['action'=>'index']);
+        }
     }
 
 }
